21d9defc06324958f3de197808f62225f64a68b7
Mithra Perera <mithraperera@hotmail.com>
2021-02-07 01:36:30 -0500

    frontend updates and about popup addition

--- frontend/stonk_hack_flutter/lib/main.dart
+++ frontend/stonk_hack_flutter/lib/main.dart
@@ -4,6 +4,11 @@ import 'dart:convert';
 
 import 'stock.dart';
 
+const Color FOREGROUND_COLOR = Color(0xff009688);
+const Color BACKGROUND_COLOR = Color(0xffFFEFD6);
+const Color SECONDARY_COLOR = Color(0xff004D40);
+const Color TERTIARY_COLOR = Color(0xff6f615B);
+
 void main() => runApp(MaterialApp(title: 'Stonk Hack 4', home: Home()));
 
 class Home extends StatefulWidget {
@@ -12,10 +17,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  static const Color FOREGROUND_COLOR = Color(0xff009688);
-  static const Color BACKGROUND_COLOR = Color(0xffFFEFD6);
-  static const Color SECONDARY_COLOR = Color(0xff004D40);
-  static const Color TERTIARY_COLOR = Color(0xff6f615B);
+  // static const Color FOREGROUND_COLOR = Color(0xff009688);
+  // static const Color BACKGROUND_COLOR = Color(0xffFFEFD6);
+  // static const Color SECONDARY_COLOR = Color(0xff004D40);
+  // static const Color TERTIARY_COLOR = Color(0xff6f615B);
 
   List<Stock> stockList = [];
 
@@ -54,18 +59,18 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         centerTitle: false,
         backgroundColor: FOREGROUND_COLOR,
-        elevation: 0.0,
+        elevation: 20.0,
         title: Row(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             Image.asset(
               'assets/5843.png',
               fit: BoxFit.cover,
-              height: 35.0,
+              height: 45.0,
             ),
             Padding(
               padding: const EdgeInsets.all(10.0),
-              child: Center(child: Text('SentimentStocks')),
+              child: Center(child: Text('SentimentStocks', style: TextStyle(fontWeight: FontWeight.bold),)),
             ),
           ],
         ),
@@ -76,7 +81,7 @@ class _HomeState extends State<Home> {
             child: Padding(
               padding: const EdgeInsets.all(20.0),
               child: Text(
-                'Top Stocks',
+                'Watchlist',
                 style: TextStyle(
                     fontSize: 40,
                     fontWeight: FontWeight.bold,
@@ -84,6 +89,10 @@ class _HomeState extends State<Home> {
                     letterSpacing: 2.0),
               ),
             ),
+          ),
+          Divider(
+            height: 20.0,
+            color: Colors.black,
           ),
           DataTable(
               columns: [
@@ -165,10 +174,92 @@ class _HomeState extends State<Home> {
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
         child: Text('?'),
         backgroundColor: SECONDARY_COLOR,
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) => _buildPopupDialog(context),
+          );
+        },
       ),
     );
   }
+}
+
+Widget _buildPopupDialog(BuildContext context) {
+  return new AlertDialog(
+    title: const Text(
+      'How does it work?',
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        color: SECONDARY_COLOR,
+      ),
+    ),
+    content: new Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          "Covid Score",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: FOREGROUND_COLOR,
+          ),
+        ),
+        Text(
+            "We provide our customers with a metric that indicates how at risk a stock is to the effects of COVID-19 through our advanced statiscal algorithms that crunch the numbers so you don't have to. We do the heavy lfiting to keep our clients safe from the uncertainity of the time and hope to keep our clients healthy in mind, body and wallet!"),
+        Text(
+          "Scale: ",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: TERTIARY_COLOR,
+          ),
+        ),
+        Text(
+          "1(GOOD) to 5(HIGH RISK)",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: TERTIARY_COLOR,
+          ),
+        ),
+        SizedBox(height: 20.0),
+        Text(
+          "Sentiment Score",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: FOREGROUND_COLOR,
+          ),
+        ),
+        Text(
+            "With the increase in retail investors in the market from 9 year olds to teenage boys looking to gamble away their birthday money, the stock market has changed and can no longer be analyzed by technical analysis alone. Using our state of the art machine learning and natural language processing models, we are able to acurately determine the public sentiment of a particular stock to help our users determine whether they want to add a stock to their portfolio."),
+        Text(
+          "1(GOOD) to 5(HIGH RISK)",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: TERTIARY_COLOR,
+          ),
+        ),
+        SizedBox(height: 20.0),
+        Text(
+          "Our Recommendation",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: FOREGROUND_COLOR,
+          ),
+        ),
+        Text(
+            "We give our recommendation based on the analysis of both the COVID and Sentiment scores. However, SentimentStocks takes no liability for the success or failure you get through our recommendations."),
+      ],
+    ),
+    actions: <Widget>[
+      new FlatButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        textColor: Theme.of(context).primaryColor,
+        child: const Text('Close'),
+      ),
+    ],
+  );
 }
